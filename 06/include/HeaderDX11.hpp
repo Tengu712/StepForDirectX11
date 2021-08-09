89ea40e4cc073854a31084ab7d036d7f58f3223b
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2021-08-10 01:07:44 +0900

    06

---
+++ 06/include/HeaderDX11.hpp
@@ -0,0 +1,97 @@
+#pragma once
+#ifndef _HEADER_HPP_
+#define _HEADER_HPP_
+
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <windows.h>
+#include <wrl/client.h>
+
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
+using Microsoft::WRL::ComPtr;
+
+struct Vertex {
+    float pcnu[10];
+};
+
+struct ConstantBuffer {
+    DirectX::XMFLOAT4X4 matScl;
+    DirectX::XMFLOAT4X4 matRot;
+    DirectX::XMFLOAT4X4 matTrs;
+    DirectX::XMFLOAT4X4 matView;
+    DirectX::XMFLOAT4X4 matProj;
+    DirectX::XMFLOAT4 vecColor;
+    DirectX::XMFLOAT4 vecLight;
+};
+
+struct Camera {
+    float width, height;
+    float angle, nearZ, farZ;
+    float posX, posY, posZ;
+    float dirX, dirY, dirZ;
+    float uppX, uppY, uppZ;
+};
+
+struct D3DInf {
+    // General
+    ComPtr<ID3D11Device> pDevice;
+    ComPtr<ID3D11DeviceContext> pImContext;
+    ComPtr<IDXGISwapChain> pSwapChain;
+    ComPtr<ID3D11RenderTargetView> pRTView;
+    ComPtr<ID3D11DepthStencilView> pDSView;
+    D3D11_VIEWPORT viewport;
+
+    // Shader
+    ComPtr<ID3D11VertexShader> pVShader;
+    ComPtr<ID3D11PixelShader> pPShader;
+
+    // Polygon
+    ComPtr<ID3D11InputLayout> pILayout;
+    ComPtr<ID3D11Buffer> pCBuffer;
+    ConstantBuffer cbuffer;
+};
+
+struct ModelInf {
+    unsigned int numIdx;
+    float posX, posY, posZ;
+    float degX, degY, degZ;
+    float sclX, sclY, sclZ;
+    float colR, colG, colB, colA;
+    ComPtr<ID3D11Buffer> pVBuffer;
+    ComPtr<ID3D11Buffer> pIBuffer;
+};
+
+class D3DManager {
+private:
+    HWND hWnd;
+    D3DInf inf;
+
+public:
+    D3DManager();
+
+    // General
+    bool init(HINSTANCE hInst, int cmdShow, LPCWSTR wndName, LPCWSTR wndClassName, unsigned int width,
+        unsigned int height, bool windowed);
+    void drawBegin();
+    void drawEnd();
+    HWND* getWindowHandle();
+    D3DInf* getD3DInformation();
+
+    // Camera
+    void createCamera(float width, float height, Camera* camera);
+    void applyCamera(Camera* camera);
+
+    // Model
+    bool createModelBuffers(unsigned int numVtx, Vertex* data, unsigned int* dataIdx, ModelInf* minf);
+    void drawModel(ModelInf* minf);
+
+    // Debug
+    void DebugBox(int i);
+    void DebugBox(const char* str);
+};
+
+#endif
+
---
+++ 06/src/Main.cpp
@@ -0,0 +1,43 @@
+﻿#include "../include/HeaderApp.hpp"
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPInst, LPSTR pCmd, int cmdShow) {
+
+    LPCWSTR kNameWnd = L"Test DX11";
+    LPCWSTR kNameWndClass = L"DX11_CLASS";
+    const unsigned int kWidth = 1920U;
+    const unsigned int kHeight = 1080U;
+    const bool kWindowed = MessageBoxW(nullptr, L"フルスクリーンで起動しますか", L"確認", MB_YESNO) == IDNO;
+
+    D3DManager dmanager = D3DManager();
+    if (!dmanager.init(hInst, cmdShow, kNameWnd, kNameWndClass, kWidth, kHeight, kWindowed))
+        return 1;
+
+    Camera camera = Camera();
+    dmanager.createCamera((float)kWidth, (float)kHeight, &camera);
+    camera.posZ = -500.0f;
+    dmanager.applyCamera(&camera);
+
+    ModelCube model = ModelCube();
+    if(!model.init(&dmanager))
+        return 1;
+
+    MSG msg;
+    while (true) {
+        if (PeekMessageW(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+            if (msg.message == WM_QUIT)
+                break;
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        } else {
+            model.degX++;
+            model.degY++;
+            dmanager.drawBegin();
+            dmanager.drawModel(&model);
+            dmanager.drawEnd();
+        }
+    }
+
+    UnregisterClassW(kNameWndClass, hInst);
+    return 0;
+}
+
